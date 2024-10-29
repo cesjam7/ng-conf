32bcc2d7d3d2ef87ad559d1d48ce096082ded0c5
Cesar Aquino <cesar7492@gmail.com>
2024-10-29 18:43:00 -0500

    angular-ng-conf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-conf';
+  data: any = {};
+  
+  ngOnInit(): void {
+    // Obtén el contenido de <noscript> con id="app-data"
+    const dataElement = document.getElementById('app-data');
+    if (dataElement) {
+      try {
+        // Parsea el contenido de <noscript> como JSON
+        this.data = JSON.parse(dataElement.textContent || '{}');
+        console.log('Datos del json:', this.data); // Para depuración
+      } catch (error) {
+        console.error('Error al parsear JSON:', error);
+      }
+    }
+  }
 }
